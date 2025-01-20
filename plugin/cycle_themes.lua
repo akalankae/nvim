4612aa3d033f121b5946ccc3266e788c8f90be89
Akalanka Edirisinghe <akalankae@gmail.com>
2025-01-20 11:39:35 +1100

    fix: fix cycle_theme_next() `next_i` getting invalid index 0

--- plugin/cycle_themes.lua
+++ plugin/cycle_themes.lua
@@ -14,7 +14,11 @@ end
 
 local function cycle_theme_next()
     curr_i = curr_i or 1 -- handle non-initialized state
-    local next_i = (curr_i+1) % NUM_THEMES -- cycle [1,NUM_THEMES]
+    local next_i = curr_i + 1
+    -- avoid NUM_THEMES = 0 (in lua index 0 is invalid)
+    if (next_i ~= NUM_THEMES) then
+        next_i = next_i % NUM_THEMES
+    end
     local next_theme = themes[next_i]
     local success, _ = pcall(vim.cmd, "colorscheme " .. next_theme)
     announce(success, next_theme)
@@ -23,6 +27,7 @@ end
 
 local function cycle_theme_prev()
     curr_i = curr_i or 1 -- handle non-initialized state
+    -- avoid NUM_THEMES = 0 (in lua index 0 is invalid)
     local prev_i = curr_i + (NUM_THEMES - 1)
     if (prev_i ~= NUM_THEMES) then
         prev_i = prev_i % NUM_THEMES
