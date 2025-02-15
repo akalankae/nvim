57efe086554c0a1d903069477cf3ce35e0d27953
Akalanka Edirisinghe <akalankae@gmail.com>
2025-02-15 17:09:00 +1100

    feat: make telescope complain if "ripgrep" program is not installed

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -10,6 +10,11 @@ require("telescope").setup()
 local nnoremap = require("user.util").normal_noremap
 local telescope = require("telescope.builtin")
 
+-- ripgrep is required for live grep function of telelscope, install if not installed
+if vim.fn.executable("rg") ~= 1 then
+  vim.notify("Install ripgrep with `pacman -S ripgrep` for live grep functionality")
+end
+
 nnoremap("<leader>fs", telescope.live_grep, { desc="[f]ind [s]tring" })
 nnoremap("<leader>ff", telescope.find_files, { desc="[f]ind [f]ile" })
 nnoremap("<leader>fb", telescope.buffers, { desc="[f]ind [b]uffer" })
