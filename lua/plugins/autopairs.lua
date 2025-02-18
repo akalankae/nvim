cd01d8f3c4a166bdb9d8e4d3f5a07dca1f51eb94
Akalanka Edirisinghe <akalankae@gmail.com>
2025-02-19 10:13:31 +1100

    feat: add autopairs (windwp)

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,9 @@
+--============================================================================
+--                          Autopairs Config
+--============================================================================
+-- Source: https://github.com/windwp/nvim-autopairs?tab=readme-ov-file
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  opts = {},
+}
