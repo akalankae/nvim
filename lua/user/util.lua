5043ca0d17056a05494ba602ae10d389b397efe9
Akalanka Edirisinghe <akalankae@gmail.com>
2025-02-18 12:06:32 +1100

    feat: add utility funcs - get abs-path of executing file & check plugin is installed

--- lua/user/util.lua
+++ lua/user/util.lua
@@ -21,4 +21,27 @@ function utils.visual_noremap(lhs, rhs, opts)
   vim.keymap.set("v", lhs, rhs, opts)
 end
 
+-- Get absolute path of currently executing block of code.
+-- stack_level 0 gets info about `debug.getinfo()` itself
+-- stack_level 1 gets info about function calling `getinfo()` (1 stack above)
+-- stack_level 2 gets info about any function calling `get_curr_file_path()` (2
+-- stacks above)
+function utils.get_curr_file_path(stack_level)
+  local info = debug.getinfo(stack_level, "S")
+  local src = info.source:gsub("^@", "")
+  return src
+end
+
+-- Search list of plugins to check whether name of config filename matches one
+-- of the installed plugins. (returns matched plugin name string)
+-- NOTE: needs some "magic" to escape hyphen in file names ("a-b" -> "a%-b")
+function utils.plugin_found(config_path)
+  local basename = vim.fs.basename(config_path)
+  for _, plugin in ipairs(PLUGINS.LIST) do
+    if basename:match(vim.pesc(plugin)) then
+      return plugin
+    end
+  end
+end
+
 return utils
