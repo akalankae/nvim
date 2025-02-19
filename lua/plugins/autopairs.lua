1cb4ed3e0e1a0bc12031211bfd36dd6f95e04423
Akalanka Edirisinghe <akalankae@gmail.com>
2025-02-19 19:57:02 +1100

    fix: remove nvim-autopairs - this is a piece of shit!

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,9 +1,5 @@
 --============================================================================
 --                          Autopairs Config
 --============================================================================
--- Source: https://github.com/windwp/nvim-autopairs?tab=readme-ov-file
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  opts = {},
 }
