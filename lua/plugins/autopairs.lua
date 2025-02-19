4675ba8c663b218556e24b0573bf3f858810491c
Akalanka Edirisinghe <akalankae@gmail.com>
2025-02-19 20:00:41 +1100

    feat: add auto-pairs (by Jiangmiao)
    Old vimscript plugin with minimal configuration and options but with all
    the necessary functionality/configuration. Unfortunately cannot be lazy
    loaded. What do they say: "Old is Gold"...

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,15 @@
 --============================================================================
---                          Autopairs Config
+--                          Auto-pairs Config
 --============================================================================
+-- Jiangmiao's auto-pairs (written in vimscript) is still the best!
+-- source: https://github.com/jiangmiao/auto-pairs
+-- NOTE: Because this is a vimscript plugin this does not support lazy loading
 return {
+  {
+    "jiangmiao/auto-pairs",
+    lazy = false,
+    config = function()
+      vim.g.AutoPairsFlyMode = 1
+    end,
+  },
 }
