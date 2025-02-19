dde8728c98f457a130eefbc851e983031edfcccd
Akalanka Edirisinghe <akalankae@gmail.com>
2025-02-19 20:03:04 +1100

    feat(need fixing): add treesitter (but configs are not loaded!)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,27 @@
 --============================================================================
 --                          Treesitter Config
 --============================================================================
+-- Source: https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      highlight = { enable = true },
-
-      ensure_installed = { "lua", "bash", "python", "c" },
-
-      additional_vim_regex_highlighting = false,
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    -- config = require("nvim-treesitter.configs").setup({
+    --   ensure_installed = { "lua", "bash", "python", "c" },
+    --   sync_install = false,
+    --   auto_install = true,
+    --   highlight = {
+    --     enable = true,
+    --   },
+    -- }),
+    opts = {
+      ensure_installed = { "lua", "c", "bash", "python" },
+      sync_install = false,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    },
+  },
 }
-
