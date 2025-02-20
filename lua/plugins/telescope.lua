17761f67808fd97096c6ef1e81656cc4edac2257
Akalanka Edirisinghe <akalankae@gmail.com>
2025-02-21 10:01:47 +1100

    feat: add telescope-keymaps for seaching telescope.builtins & editing nvim config
    * Edit neovim config directory from anywhere in the filesystem
    * Sift through telescope builtin functions (pickers, sorters, etc)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,6 +17,12 @@ return {
       map("<leader>fh", builtin.help_tags, { desc = "[f]ind [h]elp" })
       map("<leader>fc", builtin.colorscheme, { desc = "[f]ind [c]olorscheme" })
       map("<leader>fs", builtin.live_grep, { desc = "[f]ind [s]tring" })
+      -- Sift through telescope builtin functions (pickers, sorters, ... etc)
+      map("<leader>lb", builtin.builtin, { desc = "[l]ookup [b]uiltin telescope functions" })
+      -- Edit neovim config from anywhere
+      map("<leader>en", function()
+        builtin.find_files({ cwd = vim.fn.stdpath("config") }) -- ? .themes.get_ivy()
+      end, { desc = "[e]dit [n]eovim configuration" })
     end,
     opts = {
       defaults = {
