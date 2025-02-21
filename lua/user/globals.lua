a0364e21f0c68b2bfbcecb1b9ffc68c6f4beb3cf
Akalanka Edirisinghe <akalankae@gmail.com>
2025-02-22 09:33:42 +1100

    doc: explain what globals config is needed

--- lua/user/globals.lua
+++ lua/user/globals.lua
@@ -2,6 +2,9 @@
 
 local plugin_mgr = "lazy" -- TODO/IDEA: ? configurable plugin manager
 
+-- NOTE: contains the list of plugins at start up time, during session new
+-- plugins maybe installed and previous plugins maybe uninstalled, but this list
+-- remains the same.
 PLUGINS = {
   DIR = vim.fs.joinpath(vim.fn.stdpath("data"), plugin_mgr),
   LIST = {},
