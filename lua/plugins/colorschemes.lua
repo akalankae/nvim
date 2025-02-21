dd60ab27f2d18670a2787ca81042a8964e5b752e
Akalanka Edirisinghe <akalankae@gmail.com>
2025-02-22 09:39:17 +1100

    feat: elevate priority of all colorschemes to 100 (from default 50)

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,18 +1,35 @@
 --============================================================================
 --                          vim colorschemes
 --============================================================================
-return {
-  -- Theme based on Google's material design
-  { "NLKNguyen/papercolor-theme", lazy = false },
+local colorschemes = {
+    -- Theme based on Google's material design
+    { "NLKNguyen/papercolor-theme", lazy = false },
 
-  -- dark (charcoal) low-contrast theme (DEFAULT)
-  {
-    "bluz71/vim-moonfly-colors", lazy = false,
-    config = function()
-      vim.cmd [[colorscheme moonfly]]
-    end,
+    -- dark (charcoal) low-contrast theme (DEFAULT)
+    { "bluz71/vim-moonfly-colors" },
+    { "ofirgall/ofirkai.nvim" },
+    { "scottmckendry/cyberdream.nvim" }, -- high-contrast Cyberpunk vibes!
+    {
+      "marko-cerovac/material.nvim",
+      init = function()
+        vim.g.material_style = "deep ocean"
+        vim.cmd [[colorscheme material]]
+      end,
+      opts = {
+        high_visibility = {
+          lighter = true,
+        }
+      },
   },
+    { "tomasr/molokai" },
+  }
 
-  { "ofirgall/ofirkai.nvim" },
-  { "scottmckendry/cyberdream.nvim" }, -- high-contrast Cyberpunk vibes!
-}
+-- NOTE: all colorschemes should have highest priority (default=50)
+vim.tbl_map(
+  function(tbl_value)
+    tbl_value.priority = 100
+  end,
+    colorschemes
+)
+
+return colorschemes
