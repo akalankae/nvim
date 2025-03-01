feb96ad2443d4aadac3d3d1c2f1e47abaacc91c4
Akalanka Edirisinghe <akalankae@gmail.com>
2025-03-02 08:59:25 +1100

    feat: change default colorscheme to `cyberdream`

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,15 +2,9 @@
 --                          vim colorschemes
 --============================================================================
 local colorschemes = {
-    -- Theme based on Google's material design
-    { "NLKNguyen/papercolor-theme", lazy = false },
+  -- Theme based on Google's material design
+  { "NLKNguyen/papercolor-theme", lazy = false },
 
-    -- dark (charcoal) low-contrast theme (DEFAULT)
-    { "bluz71/vim-moonfly-colors" },
-    { "ofirgall/ofirkai.nvim" },
-    { "scottmckendry/cyberdream.nvim" }, -- high-contrast Cyberpunk vibes!
-    {
-      "marko-cerovac/material.nvim",
       init = function()
         vim.g.material_style = "deep ocean"
         vim.cmd [[colorscheme material]]
@@ -19,17 +13,28 @@ local colorschemes = {
         high_visibility = {
           lighter = true,
         }
-      },
+  -- dark (charcoal) low-contrast theme (DEFAULT)
+  { "bluz71/vim-moonfly-colors" },
+  { "ofirgall/ofirkai.nvim" },
+  {
+    "scottmckendry/cyberdream.nvim",
+    config = function()
+      vim.cmd [[colorscheme cyberdream]]
+    end,
+  }, -- high-contrast Cyberpunk vibes!
+  {
+    "marko-cerovac/material.nvim",
+    },
   },
-    { "tomasr/molokai" },
-  }
+  { "tomasr/molokai" },
+}
 
 -- NOTE: all colorschemes should have highest priority (default=50)
 vim.tbl_map(
   function(tbl_value)
     tbl_value.priority = 100
   end,
-    colorschemes
+  colorschemes
 )
 
 return colorschemes
