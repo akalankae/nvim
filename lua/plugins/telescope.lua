c42857826e52bd939b9d4dd3b256248389fbe2ce
Akalanka Edirisinghe <akalankae@gmail.com>
2025-04-28 22:38:17 +1000

    chore: formatting changes & disable <Ctrl>-<h> for which-key plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,16 +6,17 @@ local telescope = require "telescope.builtin"
 
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
     dependencies = {
-      'nvim-lua/plenary.nvim',
+      "nvim-lua/plenary.nvim",
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     init = function()
       nnoremap("<leader>ff", telescope.find_files, { desc = "[f]ind [f]ile" })
       nnoremap("<leader>fh", telescope.help_tags, { desc = "[f]ind [h]elp" })
       nnoremap("<leader>fc", telescope.colorscheme, { desc = "[f]ind [c]olorscheme" })
-      nnoremap("<leader>fb", telescope.buffers, { desc="[f]ind [b]uffer" })
+      nnoremap("<leader>fb", telescope.buffers, { desc = "[f]ind [b]uffer" })
       nnoremap("<leader>fs", telescope.live_grep, { desc = "[f]ind [s]tring" })
       -- Sift through telescope builtin functions (pickers, sorters, ... etc)
       nnoremap("<leader>ft", telescope.builtin, { desc = "[f]ind builtin [t]elescope function" })
@@ -24,19 +25,19 @@ return {
         telescope.find_files({ cwd = vim.fn.stdpath("config") }) -- ? .themes.get_ivy()
       end, { desc = "[e]dit [n]eovim configuration" })
       nnoremap("<leader>fd", function()
-        if telescope.diagnostics then
-          telescope.diagnostics()
-        else
-          vim.notify("Cannot find `telescope.diagnostics()`", vim.log.levels.INFO)
-        end
-      end,
-        { desc="[f]ind [d]iagnostic msg" })
+          if telescope.diagnostics then
+            telescope.diagnostics()
+          else
+            vim.notify("Cannot find `telescope.diagnostics()`", vim.log.levels.INFO)
+          end
+        end,
+        { desc = "[f]ind [d]iagnostic msg" })
     end,
     opts = {
       defaults = {
         mappings = {
           i = {
-            ["<C-h>"] = "which-key",
+            -- ["<C-h>"] = "which-key",
           },
           n = {
           },
