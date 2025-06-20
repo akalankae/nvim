cd1caccd433b9becd96bc172aa87fc351044ca85
Akalanka Edirisinghe <akalankae@gmail.com>
2025-06-20 15:19:05 +1000

    feat: add keymaps to select inside and outside functions with `af` and `if` keys

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -29,6 +29,10 @@ return {
         select = {
           enable = true,
           lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner"
+          },
         },
       },
       move = {
