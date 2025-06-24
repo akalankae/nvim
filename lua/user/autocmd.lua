c1a4aef3b364187befd3e1ed93c6549a85bf953c
Akalanka Edirisinghe <akalankae@gmail.com>
2025-06-24 14:07:21 +1000

    feat: add 2 autocmds to remember last used colorscheme and autoload it on restart

--- lua/user/autocmd.lua
+++ lua/user/autocmd.lua
@@ -40,13 +40,63 @@ create_autocmd("BufReadPost", {
   end,
 })
 
---=============================================================================
+--==============================================================================
 -- Register user commands to switch background color between Dark and Light
---=============================================================================
+--==============================================================================
 create_autocmd("VimEnter", {
   pattern = "*",
   callback = function()
     create_user_command("Dark", "set bg=dark", { bang = true })
     create_user_command("Light", "set bg=light", { bang = true })
   end,
+})
+
+--==============================================================================
+-- Restore last used colorscheme
+--==============================================================================
+-- We save the colorscheme used on exit of a session to a file, then reload that
+-- colorscheme from the file when a new session starts. Colorscheme is recorded
+-- in ~/.local/state/nvim/colorscheme.dat
+local colorscheme_file = vim.fn.stdpath('state') .. "/colorscheme.dat"
+local autoload_colorscheme = create_augroup("AutoloadColorscheme", opts)
+
+create_autocmd("VimLeave", { pattern = "*", group = autoload_colorscheme,
+  callback = function()
+    local code, errmsg = vim.fn.writefile({vim.g.colors_name}, colorscheme_file)
+    if code == -1 then
+      vim.notify("Failed to write to colorscheme file: " .. errmsg, vim.log.levels.ERROR)
+    end
+  end,
+  desc = "Record last colorscheme"
+})
+
+
+-- Make sure colorscheme data file exists and it contains the name of a valid
+-- colorscheme, if not fallback to a default colorscheme.
+local function load_last_colorscheme(fallback)
+  local available_colorschemes = vim.fn.getcompletion("", "color")
+  local colorscheme = fallback or "default"
+  local filedata, err = vim.uv.fs_stat(colorscheme_file)
+  -- If colorscheme file was found and it gives a valid colorscheme use it
+  if filedata then
+    local last_colorscheme = vim.fn.readfile(colorscheme_file)[1]
+    if last_colorscheme and vim.tbl_contains(available_colorschemes, last_colorscheme) then
+      colorscheme = last_colorscheme
+    else
+      vim.notify("Could not find colorscheme: " .. last_colorscheme, vim.log.levels.ERROR)
+    end
+  else
+    vim.notify(err, vim.log.levels.ERROR)
+  end
+  vim.cmd("colorscheme " .. colorscheme)
+  vim.notify("Loading colorscheme: " .. colorscheme, vim.log.levels.INFO)
+end
+
+create_autocmd("VimEnter", {
+  pattern = "*",
+  group = autoload_colorscheme,
+  callback = function()
+    load_last_colorscheme("default")
+  end,
+  desc = "Load last used colorscheme"
 })
