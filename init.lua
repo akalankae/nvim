5f7ba58227759e116aaf114fa9f20ed731e4414f
Akalanka Edirisinghe <akalankae@gmail.com>
2025-06-24 12:45:53 +1000

    style: remove parentheses from require calls

--- init.lua
+++ init.lua
@@ -3,10 +3,12 @@
 -- 		Main configuration file for neovim in lua
 --=============================================================================
 
-require("user.settings")
-require("user.keymap")
-require("user.autocmd")
+-- configurations related to neovim core
+require "user.settings"
+require "user.keymap"
+require "user.autocmd"
 
-require("plugins")
-require("plugins.treesitter")
-require("plugins.lualine")
+-- configurations related to installed plugins
+require "plugins"
+require "plugins.treesitter"
+require "plugins.lualine"
