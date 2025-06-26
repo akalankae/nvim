edd9bcefcc3c6b9a36125cfb835bce2ccceaf93b
Akalanka Edirisinghe <akalankae@gmail.com>
2025-06-27 01:46:25 +1000

    refactor: move configs for all colorschemes to one file

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,17 @@
+-- IMPORTANT: you can CYCLE THROUGH all available colorschemes using custom keymappings
+-- <ALT-n> (go to next) and <ALT-p> (go to previous)
+
+return {
+  -- Based on Google's material design
+  { "NLKNguyen/papercolor-theme",  priority = 1000 },
+
+  { "dasupradyumna/midnight.nvim", priority = 1000 }, -- dark, low-contrast
+  { "scottmckendry/cyberdream.nvim", priority = 1000 }, -- dark, futuristic, high-contrast
+
+  -- Based on Atom: onedark, onelight, onedark_vivid, onedark_dark, vaporwave
+  { "olimorris/onedarkpro.nvim", priority = 1000 },
+
+  { "bluz71/vim-moonfly-colors", name = "moonfly", priority = 1000 },
+  { "vague2k/vague.nvim", priority = 1000 },
+  { "zootedb0t/citruszest.nvim", priority = 1000 }
+}
