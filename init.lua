da7cf40ce9bd26d3b43988519023f6c2fbe7f45a
Akalanka Edirisinghe <akalankae@gmail.com>
2025-06-27 01:45:29 +1000

    refactor: source lua/user/plugins.lua instead of lua/plugins/init.lua

--- init.lua
+++ init.lua
@@ -7,8 +7,4 @@
 require "user.settings"
 require "user.keymap"
 require "user.autocmd"
-
--- configurations related to installed plugins
-require "plugins"
-require "plugins.treesitter"
-require "plugins.lualine"
+require "user.plugins"
