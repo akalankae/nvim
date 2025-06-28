482a05b7d8f8d0cf6ab3ed0f5001102c8b492d88
Akalanka Edirisinghe <akalankae@gmail.com>
2025-06-28 17:18:47 +1000

    feat: add ripgrep, fzf to aid telescope search features

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,11 @@ return {
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
   dependencies = {
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
+    "BurntSushi/ripgrep",
+    { "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release"
+    }
   },
   keys = {
     { "<leader>fs", require("telescope.builtin").live_grep, desc = "[f]ind [s]tring" },
