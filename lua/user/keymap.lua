885e732666be06cd28223da963daacc9613cb72f
Akalanka Edirisinghe <akalankae@gmail.com>
2025-11-10 23:43:16 +1100

    feat: add keymap to quickly close quickfix-list buffers

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -4,8 +4,8 @@
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 local util = require "user.util"
 
--- Setup global LEADER key
-vim.g.mapleader = ";"
+-- Setup SPACEBAR as global LEADER key
+vim.g.mapleader = " "
 
 -- Close window with ctrl-q
 util.nnoremap("<C-q>", "<C-w>q")
@@ -59,25 +59,9 @@ util.nnoremap("k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 util.nnoremap("j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- ────────────────────────────────────────────────────────────────────────────
--- Launch toggable terminal session below running neovim instance
+-- Launch and move to terminal session below editor session
 -- ────────────────────────────────────────────────────────────────────────────
--- user/autocmd.lua: "TermOpen" disables line numbers
--- :term or :terminal opens a terminal session on a new buffer
--- You will initially be in normal mode (i.e. you cannot enter text into the
--- terminal).  Press "i", "I", "a", "A" or :startinsert to enter Terminal mode.
--- (cursor moves to end of shell prompt) In Terminal mode all keystrokes go to
--- underlying program, EXCEPT when CTRL-\ is entered.
--- When you press CTRL-\ + CTRL-n you enter normal mode,
--- when you press CTRL-\ + CTRL-o you enter normal mode for ONE command only
--- and then go straight back to Terminal mode.
--- Any other key pressed after CTRL-\ go to the underlying program.
-util.nnoremap("<Leader>t", function()
-  vim.cmd.new()         -- create new horizontal split window
-  vim.cmd.terminal()
-  vim.cmd.wincmd("J")   -- move current window to lowermost area
-  vim.api.nvim_win_set_height(0, 15)
-  vim.cmd.startinsert() -- go to Terminal-mode straight away
-end)
+util.nnoremap("<Leader>t", "<Plug>(LaunchToggleTerm)")
 
 -- ────────────────────────────────────────────────────────────────────────────
 -- Quick escape to NORMAL mode from TERMINAL mode
@@ -85,3 +69,13 @@ end)
 -- In TERMINAL-JOB mode (to which you get after "i" or "a" in TERMINAL mode) and
 -- TERMINAL mode double tapping Escape key is akin to <CTRL-\> + <CTRL-n>
 vim.keymap.set("t", "<Esc><Esc>","<C-\\><C-n>")
+
+-- ────────────────────────────────────────────────────────────────────────────
+-- Close quickfix lists with `q`
+-- ────────────────────────────────────────────────────────────────────────────
+vim.keymap.set("n", "q", function()
+  if vim.o.filetype == "qf" then
+    vim.cmd [[quit]]
+  end
+end,
+{ desc="close quickfix lists with `q`" })
