3c71580db1be1d11765abad428b3513e91a558d6
Akalanka Edirisinghe <akalankae@gmail.com>
2025-11-22 14:49:44 +1100

    fix: fix changes from version control mishaps

--- lua/user/autocmd.lua
+++ lua/user/autocmd.lua
@@ -134,3 +134,40 @@ create_autocmd("VimEnter", {
   callback = load_last_colorscheme,
   desc = "Load last used colorscheme on startup"
 })
+
+-- From "nvim-lua/kickstart.nvim"
+-- Make j/k move up/down using "visual" lines instead of actual lines, even when
+-- lines are wrapped with window splitting, when motion is not prepended by a
+-- count (e.g. k, not with 10k). Uses gk/gj instead of k/j, respectively, when
+-- a line count is not given.
+local nnoremap = require("user.util").nnoremap
+nnoremap("k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+nnoremap("j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- ────────────────────────────────────────────────────────────────────────────
+-- Launch toggable terminal session below running neovim instance
+-- ────────────────────────────────────────────────────────────────────────────
+-- user/autocmd.lua: "TermOpen" disables line numbers
+-- :term or :terminal opens a terminal session on a new buffer
+-- You will initially be in normal mode (i.e. you cannot enter text into the
+-- terminal).  Press "i", "I", "a", "A" or :startinsert to enter Terminal mode.
+-- (cursor moves to end of shell prompt) In Terminal mode all keystrokes go to
+-- underlying program, EXCEPT when CTRL-\ is entered.
+-- When you press CTRL-\ + CTRL-n you enter normal mode,
+-- when you press CTRL-\ + CTRL-o you enter normal mode for ONE command only
+-- and then go straight back to Terminal mode.
+-- Any other key pressed after CTRL-\ go to the underlying program.
+nnoremap("<Leader>t", function()
+  vim.cmd.new()         -- create new horizontal split window
+  vim.cmd.terminal()
+  vim.cmd.wincmd("J")   -- move current window to lowermost area
+  vim.api.nvim_win_set_height(0, 15)
+  vim.cmd.startinsert() -- go to Terminal-mode straight away
+end)
+
+-- ────────────────────────────────────────────────────────────────────────────
+-- Quick escape to NORMAL mode from TERMINAL mode
+-- ────────────────────────────────────────────────────────────────────────────
+-- In TERMINAL-JOB mode (to which you get after "i" or "a" in TERMINAL mode) and
+-- TERMINAL mode double tapping Escape key is akin to <CTRL-\> + <CTRL-n>
+vim.keymap.set("t", "<Esc><Esc>","<C-\\><C-n>")
