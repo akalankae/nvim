7b9dda7abd983b813d7f5fa66df3f81706726d2a
Akalanka Edirisinghe <akalankae@gmail.com>
2025-12-07 02:08:24 +1100

    feat: add current time and LSP status to lualine statusbar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -26,8 +26,8 @@ return {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_c = {'filename', {'datetime', style = '%d %b %H:%M'}},
+    lualine_x = {'lsp_status', 'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
