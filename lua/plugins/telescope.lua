f0c1826b7606a8b60e43091bdc61a323baa3c26d
Akalanka Edirisinghe <akalankae@gmail.com>
2025-12-06 18:48:36 +1100

    fix: upgrade to current tag version and fix keymappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@
 
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  tag = "v0.2.0",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "BurntSushi/ripgrep",
@@ -12,16 +12,17 @@ return {
         build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release"
     }
   },
-  keys = {
-    { "<leader>fs", require("telescope.builtin").live_grep, desc = "[f]ind [s]tring" },
-    { "<leader>ff", require("telescope.builtin").find_files, desc = "[f]ind [f]ile" },
-    { "<leader>fb", require("telescope.builtin").buffers, desc = "[f]ind [b]uffer" },
-    { "<leader>fh", require("telescope.builtin").help_tags, desc = "[f]ind [h]help tag" },
-    { "<leader>fc", require("telescope.builtin").colorscheme, desc = "[f]ind [c]olorscheme" },
-    { "<leader>fd", require("telescope.builtin").diagnostics, desc = "[f]ind [d]iagnostic message" },
-  },
-  -- extensions = { }
-  -- config = function(lazyplugin, opts)
-  --   require("telescope").load_extension("fzf")
-  -- end,
+  opts = function(plugin_spec)
+    local telescope = require("telescope.builtin")
+    local nnoremap = require("user.util").normal_noremap
+    nnoremap("<leader>fs", telescope.live_grep, {desc = "[f]ind [s]tring"})
+    nnoremap( "<leader>ff", telescope.find_files, {desc = "[f]ind [f]ile"} )
+    nnoremap( "<leader>fb", telescope.buffers, {desc = "[f]ind [b]uffer"} )
+    nnoremap( "<leader>fh", telescope.help_tags, {desc = "[f]ind [h]help tag"} )
+    nnoremap( "<leader>fc", telescope.colorscheme, {desc = "[f]ind [c]olorscheme"} )
+    nnoremap( "<leader>fd", telescope.diagnostics, {desc = "[f]ind [d]iagnostic message"} )
+  end,
+  config = function(lazyplugin, opts)
+    require("telescope").load_extension("fzf")
+  end,
 }
