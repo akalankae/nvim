6af40e41949cd05eab9a8577e418cb908caa2d92
Akalanka Edirisinghe <akalankae@gmail.com>
2025-12-07 00:08:58 +1100

    feat: add mason.nvim plugin

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+    "mason-org/mason.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig"
+  },
+    opts = {
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
+            }
+        },
+    log_level = vim.log.levels.DEBUG
+    }
+}
