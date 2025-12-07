9a7fc7af6a58e6e47ea723b9e882086bf12caaa3
Akalanka Edirisinghe <akalankae@gmail.com>
2025-12-07 13:10:37 +1100

    feat: add more settings to lua_ls config

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -4,6 +4,20 @@ return {
   root_markers = { ".emmyrc.json", ".luarc.json", ".luacheckrc", ".stylua.toml", "stylua.toml", ".git" },
   settings = {
     Lua = {
+      runtime = {
+        version = "Lua 5.4",
+      },
+      completion = {
+        enable = true,
+      },
+      diagnostics = {
+        enable = true,
+        globals = { "vim" },
+      },
+      workspace = {
+        library = vim.env.RUNTIME,
+        checkThirdParty = false,
+      },
       codeLens = {
         enable = true,
       },
@@ -14,4 +28,3 @@ return {
     },
   },
 }
-
