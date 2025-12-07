50740376430192b0b341b415bd7b61b5b3e6eefc
Akalanka Edirisinghe <akalankae@gmail.com>
2025-12-07 13:13:23 +1100

    feat: change vim diagnostic settings
    Icons for signs instead of letters; enable sorting according to severity
    and virtual text

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -61,3 +61,16 @@ vim.lsp.enable({
   "basedpyright",
   "clangd",
 })
+
+vim.diagnostic.config({
+  virtual_text = true,
+  severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+  },
+})
