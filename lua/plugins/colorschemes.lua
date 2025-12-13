752fcc6b9e6c5dc395d11477b19cd82574c3f738
Akalanka Edirisinghe <akalankae@gmail.com>
2025-12-13 22:32:02 +1100

    cleanup: remove all but PaperColor and cyberdream colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,34 +1,8 @@
 --============================================================================
 --                          vim colorschemes
 --============================================================================
-local colorschemes = {
-  -- Theme based on Google's material design
-  { "NLKNguyen/papercolor-theme", lazy = false },
-
-  -- dark (charcoal) low-contrast theme (DEFAULT)
-  { "bluz71/vim-moonfly-colors" },
-  { "ofirgall/ofirkai.nvim" },
-  { "scottmckendry/cyberdream.nvim" }, -- high-contrast Cyberpunk vibes!
-  {
-    "marko-cerovac/material.nvim",
-    init = function()
-      vim.g.material_style = "deep ocean"
-    end,
-    opts = {
-      high_visibility = {
-        lighter = true,
-      }
-    },
-  },
-  { "tomasr/molokai" },
+return {
+  
+   "NLKNguyen/papercolor-theme",     -- Theme based on Google's material design
+   "scottmckendry/cyberdream.nvim" , -- high-contrast Cyberpunk vibes!
 }
-
--- NOTE: all colorschemes should have highest priority (default=50)
-vim.tbl_map(
-  function(tbl_value)
-    tbl_value.priority = 100
-  end,
-  colorschemes
-)
-
-return colorschemes
