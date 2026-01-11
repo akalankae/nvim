3db1b85c972668edf5df5e9e23d2439035882fb4
Akalanka Edirisinghe <akalankae@gmail.com>
2026-01-11 23:16:16 +1100

    style: change  options to

--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -1,3 +1,3 @@
-vim.o.spell = true -- turn on spell checker
-vim.o.wrap = false -- do not wrap text to next line
-vim.o.textwidth = 0 -- EOL not entered by vim until author does so
+vim.opt_local.spell = true  -- turn on spell checker
+vim.opt_local.wrap = false  -- do not wrap text to next line
+vim.opt_local.textwidth = 0 -- EOL not entered by vim until author does so
